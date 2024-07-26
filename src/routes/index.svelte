93ef9a80005cdf5a3cd3c8d976b9fc76ca35ec79
Your Name <you@example.com>
2024-07-26 11:59:15 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from 'svelte';
+  import Header from '../lib/Header.svelte';
+  import ProductList from '../lib/ProductList.svelte';
+  import { getProducts } from '../lib/api.js';
+
+  let products = [];
+
+  onMount(async () => {
+    products = await getProducts();
+  });
+</script>
+
+<Header title="My Online Shop" />
+<ProductList {products} />
+
+<style>
+  main {
+    padding: 1rem;
+  }
+</style>
